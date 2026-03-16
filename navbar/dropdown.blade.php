af91659d407096a3182b995c7124ac2692b6a462
Yohan Alek <yh9dev@gmail.com>
2026-03-16 11:31:23 -0700

    Repositorio de componentes iniciado.

---
+++ navbar/dropdown.blade.php
@@ -0,0 +1,35 @@
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 border border-base-400 dark:border-base-300 bg-base-100'])
+
+@php
+$alignmentClasses = match ($align) {
+    'left' => 'ltr:origin-top-left rtl:origin-top-right inset-s-0',
+    'top' => 'origin-top',
+    default => 'ltr:origin-top-right rtl:origin-top-left inset-e-0',
+};
+
+$width = match ($width) {
+    '48' => 'w-48',
+    default => $width,
+};
+@endphp
+
+<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+    <div @click="open = ! open">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="absolute z-50 mt-2 {{ $width }} rounded-md {{ $alignmentClasses }}"
+            style="display: none;"
+            @click="open = false">
+        <div class="rounded-md {{ $contentClasses }}">
+            {{ $content }}
+        </div>
+    </div>
+</div>
