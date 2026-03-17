d72cb939365c00441b35289a52f01521ff9c5497
Yohan Alek <yh9dev@gmail.com>
2026-03-16 18:35:09 -0700

    Texto de titulo de alerta con top-[2.6px].

--- alert.blade.php
+++ alert.blade.php
@@ -57,7 +57,7 @@
     </div>
 
     {{-- Contenido Principal --}}
-    <div class="flex-1 text-left relative top-0.75">
+    <div class="flex-1 text-left relative top-[2.6px]">
         @if ($title)
             <h3 class="text-sm font-medium">{{ $title }}</h3>
         @endif
